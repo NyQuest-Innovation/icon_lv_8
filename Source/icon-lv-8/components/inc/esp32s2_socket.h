69b39d48ccc41ef47164d7dbceeeb9c44cf4b32f
vparaprath <vparaprath@pole3.com>
2024-07-08 20:20:45 +0530

    Adding some debug prints

--- Source/icon-lv-8/components/inc/esp32s2_socket.h
+++ Source/icon-lv-8/components/inc/esp32s2_socket.h
@@ -330,7 +330,7 @@ typedef enum{
 typedef enum{
     HW_VERSION = 0x0300,
     //FW_VERSION = 0x0811, // Default version when received
-    FW_VERSION = 0x0300, // Debug version for specific customer, Refer Kiran's mail on 6th July 2024
+    FW_VERSION = 0x0207,
 }version_t;
 
 extern volatile uint16_t system_flag;
